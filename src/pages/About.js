369015885b736d957c5dd11f55e301825e027ff2
Griselandradev <grisel.andrade016@pascualbravo.edu.co>
2024-11-06 17:08:58 -0500

    primer commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -7,9 +7,8 @@ function About() {
     <div>
       <Header /> {}
       <div className="about-container">
-        <img src="/path/to/your/image.jpg" alt="About Us" /> {}
         <h1>About Us</h1>
-        <p>Welcome to our website! We are a company based in Medellín, Colombia. We strive to provide the best service to our clients. Our team is dedicated to innovation and delivering quality solutions. Thank you for visiting our site!</p>
+        <p>ESTO DEBE MEJORAR</p>
         <p><strong>Location:</strong> Medellín, Colombia</p>
         <p><strong>Year:</strong> 2024</p>
       </div>
